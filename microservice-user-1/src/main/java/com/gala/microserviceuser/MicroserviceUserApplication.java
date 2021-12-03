23bf85227c1dcb34581bbc04afeb9da3622e6e1c
GALAace <galaace@qq.com>
2021-12-03 17:04:49 +0800

    common-api pom修改

---
+++ microservice-user-1/src/main/java/com/gala/microserviceuser/MicroserviceUserApplication.java
@@ -0,0 +1,13 @@
+package com.gala.microserviceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUserApplication.class, args);
+    }
+
+}
---
+++ microservice-user-1/src/test/java/com/gala/microserviceuser/MicroserviceUser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gala.microserviceuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUser1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
